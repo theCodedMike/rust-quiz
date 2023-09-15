486e8ae7fa19de1f4af9a7fe95f8f4d800861d0b
mike <lixiaofeng1115@outlook.com>
2023-09-16 02:52:33 +0800

    rust quiz 1

---
+++ src/bin/quiz_1/rust_quiz_1.rs
@@ -0,0 +1,19 @@
+//! ## Run
+//! cargo r --bin quiz-1
+
+macro_rules! m {
+    ($($s:stmt)*) => {
+        $(
+            { stringify!($s); 1 }
+        )<<*
+    };
+}
+
+fn main() {
+    print!(
+        "{}{}{}",
+        m! { return || true },
+        m! { (return) || true },
+        m! { {return} || true },
+    );
+}
